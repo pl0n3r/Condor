a1376ca4f3ace09e2a07ccb4da9905ae3e7a512a
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-24 23:02:52 -0500

    build(static): preparar PHPStan y Rector

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    ->withPhpSets()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+    )
+    ->withComposerBased(
+        doctrine: true,
+        symfony: true,
+    );
